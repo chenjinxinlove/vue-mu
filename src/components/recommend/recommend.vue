f2ec60364b66eb4a1e8ff01a099bd241481b503b
chenjinxinlove <505150704@qq.com>
2018-03-09 23:24:55 +0800

    slider

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-content">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <Slider>
+          <div  v-for="item in recommends" :key="item.id">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
+  import {getRecommend} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+  export default {
+    data() {
+      return {
+        recommends : []
+      }
+    },
+    components: {
+      Slider
+    },
+    created() {
+      this._getRecommend();
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .recommend
+    position fixed
+    width 100%
+    top 88px
+    bottom 0
+    .recommend-content
+      height 100%
+      overflow hidden
+      .slider-wrapper
+        position relative
+        width 100%
+        overflow hidden
+</style>
+
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      singer
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+    
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+
+</style>
