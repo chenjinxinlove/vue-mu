8a4f75f45c596c3c4cfe6682ab1a4e327a2a088d
chenjinxinlove <505150704@qq.com>
2018-03-13 22:03:51 +0800

    music up

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import store from './store'
 import VueLazyLoad from 'vue-lazyload'
 
 import './common/stylus/index.styl'
@@ -19,5 +20,6 @@ Vue.use(VueLazyLoad, {
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
